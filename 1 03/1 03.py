f62f7b533fd6fb05381f077b417fd079fd373565
Alik <aliksibagatullin544@gmail.com>
2022-06-19 22:33:53 +0500

    add files

---
+++ 1 03/1 03.py
@@ -0,0 +1,8 @@
+Password = input()
+Login = input()
+Mail = input()
+if Password == "раз" and Login == "два" and Mail == "три":
+    print("ГОРИ")
+else:
+    print("НЕ ГОРИ")
+
---
+++ 1 08/1 08.py
@@ -0,0 +1,3 @@
+a = input()
+if a.find('кот')>0:print('МЯУ')
+else:print('ГАВ')
